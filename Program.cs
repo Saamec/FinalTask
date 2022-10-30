029ed0a16d3fc534ca3653c2fdbd48f2096d5e0b
«Ilya <333zero777@mail.ru>
2022-10-30 21:27:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Dynamic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+BinaryFormatter formatter = new BinaryFormatter();
+
+
+using (var fs = new FileStream("C:\\Users\\Admin\\Desktop\\Students.dat", FileMode.OpenOrCreate))
+{
+    Student [] newStudent = (Student[])formatter.Deserialize(fs);
+    Console.WriteLine("Объект десериализован");
+    //Console.WriteLine($"Имя: {newStudent.Name} --- Группа: {newStudent.Group} --- Дата рождения: {newStudent.DateOfBirth}");
+}
+Console.ReadLine();
+
+[Serializable]
+public class Student
+{
+    internal string Name { get; set; }
+    internal string Group { get; set; }
+    internal DateTime DateOfBirth { get; set; }
+
+    public Student(string name, string group, DateTime dateofbirth)
+    {
+        Name = name;
+        Group = group;
+        DateOfBirth = dateofbirth;
+    }
+}
